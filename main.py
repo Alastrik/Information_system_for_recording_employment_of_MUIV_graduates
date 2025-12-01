7bc5cdca3af4e52853464d371e0792bf4f1e52bf
Alastrik <87321748+Alastrik@users.noreply.github.com>
2025-12-01 23:22:58 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,8 @@
+# main.py
+import os
+import sys
+from django.core.management import execute_from_command_line
+
+if __name__ == '__main__':
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'employment_system.settings')
+    execute_from_command_line([sys.argv[0], 'runserver', '127.0.0.1:8000'])
